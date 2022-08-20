66b76f5d5f007e1a528f4c6e45334c6ea258764a
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-08-20 07:24:39 -0300

    Day 3 - Finished

---
+++ 03 - Calculator/js/elements.js
@@ -0,0 +1,47 @@
+const results = document.querySelector('.result')
+const history = document.querySelector('.history')
+const equals = document.querySelector('.equals')
+const split = document.querySelector('#split')
+const percent = document.querySelector('#percent')
+const bracket = document.querySelector('#bracket')
+const clear = document.querySelector('#clear')
+const zero = document.querySelector('#zero')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const add = document.querySelector('#add')
+const subtract = document.querySelector('#subtract')
+const multiply = document.querySelector('#multiply')
+const comma = document.querySelector('#comma')
+const del = document.querySelector('#del')
+
+export default {
+  results,
+  history,
+  equals,
+  split,
+  percent,
+  bracket,
+  clear,
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  add,
+  subtract,
+  multiply,
+  comma,
+  del
+}
---
+++ 03 - Calculator/script.js
@@ -0,0 +1,157 @@
+import Elements from './js/elements.js'
+
+const el = Elements
+
+el.results.textContent = ''
+let previousValue = 0
+let value = 0
+let result = 0
+let operator = ''
+
+function clearNumbers() {
+  value = 0
+  result = 0
+  previousValue = 0
+  el.results.textContent = ''
+  el.history.textContent = ''
+}
+
+function pressNumber(number) {
+  if (
+    (el.results.textContent !== 0 && result !== 0) ||
+    value == 0 ||
+    previousValue == 0
+  ) {
+    el.results.textContent = parseFloat(
+      el.results.textContent + number.textContent
+    )
+  } else if (
+    el.results.textContent !== 0 &&
+    result == 0 &&
+    value == 0 &&
+    previousValue == 0
+  ) {
+    el.results.textContent = parseFloat(number.textContent)
+  } else {
+    el.results.textContent = parseFloat(number.textContent)
+  }
+}
+
+const operators = {
+  add: '+',
+  multiply: '*',
+  split: '/',
+  subtract: '-',
+  percent: '%'
+}
+
+function operations(operator) {
+  if (!el.results.textContent && previousValue != 0) {
+    console.log(previousValue)
+    el.results.textContent = 'Digite algo'
+  } else {
+    if (value != 0 && previousValue != 0) {
+      previousValue = parseFloat(value) + parseFloat(previousValue)
+    } else {
+      previousValue = parseFloat(el.results.textContent)
+      el.results.textContent = ''
+      el.history.textContent = `${previousValue} ${operator}`
+    }
+  }
+}
+
+document.addEventListener('click', btnClicked => {
+  switch (btnClicked.target) {
+    case el.subtract:
+      operator = operators.subtract
+      break
+    case el.add:
+      operator = operators.add
+      break
+    case el.split:
+      operator = operators.split
+      break
+    case el.multiply:
+      operator = operators.multiply
+      break
+
+    default:
+      break
+  }
+})
+
+el.clear.addEventListener('click', () => {
+  clearNumbers()
+})
+
+el.zero.addEventListener('click', () => {
+  pressNumber(el.zero)
+})
+el.one.addEventListener('click', () => {
+  pressNumber(el.one)
+})
+el.two.addEventListener('click', () => {
+  pressNumber(el.two)
+})
+el.three.addEventListener('click', () => {
+  pressNumber(el.three)
+})
+el.four.addEventListener('click', () => {
+  pressNumber(el.four)
+})
+el.five.addEventListener('click', () => {
+  pressNumber(el.five)
+})
+el.six.addEventListener('click', () => {
+  pressNumber(el.six)
+})
+el.seven.addEventListener('click', () => {
+  pressNumber(el.seven)
+})
+el.eight.addEventListener('click', () => {
+  pressNumber(el.eight)
+})
+el.nine.addEventListener('click', () => {
+  pressNumber(el.nine)
+})
+
+el.add.addEventListener('click', () => {
+  operations(operators.add)
+})
+
+el.subtract.addEventListener('click', () => {
+  operations(operators.subtract)
+})
+
+el.multiply.addEventListener('click', () => {
+  operations(operators.multiply)
+})
+
+el.split.addEventListener('click', () => {
+  operations(operators.split)
+})
+
+el.percent.addEventListener('click', () => {
+  operations(operators.percent)
+})
+
+el.equals.onclick = () => {
+  value = parseFloat(el.results.textContent)
+  if (operator === '+') {
+    result = previousValue + value
+  } else if (operator === '-') {
+    result = previousValue - value
+  } else if (operator === '/') {
+    result = previousValue / value
+  } else if (operator === '*') {
+    result = previousValue * value
+  } else if (operator === '%') {
+    result = previousValue * (value / 100)
+  }
+  el.results.textContent = ''
+  el.history.textContent = `${el.history.textContent} ${value} = ${result}`
+
+  value = 0
+  result = 0
+  previousValue = 0
+}
