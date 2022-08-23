f2a1848a253cb4197b809429dedb4797d8606138
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-08-23 15:13:01 -0300

    Finished day 8 and update Readme

--- 08 - Toggle Button/index.js
+++ 08 - Toggle Button/index.js
@@ -1,5 +1,27 @@
-const theme = document.querySelector('.switch')
+const themeToggle = document.querySelector('#theme-toggle')
 
-theme.addEventListener('click', event => {
-  console.log(event)
+themeToggle.addEventListener('click', () => {
+  document.body.classList.contains('light-theme')
+    ? enableDarkMode()
+    : enableLightMode()
 })
+
+function enableDarkMode() {
+  document.body.classList.remove('light-theme')
+  document.body.classList.add('dark-theme')
+}
+
+function enableLightMode() {
+  document.body.classList.remove('dark-theme')
+  document.body.classList.add('light-theme')
+}
+
+function setThemePreference() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+    return
+  }
+  enableLightMode()
+}
+
+document.onload = setThemePreference()
